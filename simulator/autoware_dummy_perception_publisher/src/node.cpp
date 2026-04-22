f321645f882e208a59c5564fe6404068538ae48a
RyoFunai <lacerta2869@gmail.com>
2026-04-22 10:49:21 +0900

    Add minimal LiDAR perception preset and psim bridge for simulated pointcloud input

--- simulator/autoware_dummy_perception_publisher/src/node.cpp
+++ simulator/autoware_dummy_perception_publisher/src/node.cpp
@@ -92,7 +92,8 @@ DummyPerceptionPublisherNode::DummyPerceptionPublisherNode()
   detected_object_with_feature_pub_ =
     this->create_publisher<tier4_perception_msgs::msg::DetectedObjectsWithFeature>(
       "output/dynamic_object", qos);
-  pointcloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("output/points_raw", qos);
+  pointcloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+    "output/points_raw", rclcpp::SensorDataQoS().keep_last(1));
   object_sub_ = this->create_subscription<tier4_simulation_msgs::msg::DummyObject>(
     "input/object", 100,
     std::bind(&DummyPerceptionPublisherNode::objectCallback, this, std::placeholders::_1));
